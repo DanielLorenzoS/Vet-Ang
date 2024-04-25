8042a4f66f5b6bd1e943ea087f662463b6bf0353
DanielLorenzoSEP <147671627+DanielLorenzoSEP@users.noreply.github.com>
2024-04-25 16:58:40 -0600

    update pos

--- src/app/pages/pos/pos.component.ts
+++ src/app/pages/pos/pos.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
 export class PosComponent {
 
   activeButton = 1;
+  products = [
+    { id: 1, name: 'Product 1', description: "desc", price: 10 },
+    { id: 2, name: 'Product 2', description: "desc", price: 20 },
+    { id: 3, name: 'Product 3', description: "desc", price: 30 },
+    { id: 4, name: 'Product 4', description: "desc", price: 40 },
+    { id: 5, name: 'Product 5', description: "desc", price: 50 },
+  ];
   
 }
