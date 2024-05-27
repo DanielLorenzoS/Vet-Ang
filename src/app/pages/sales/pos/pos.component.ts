e2c45c3a5f54dfeb4b056ffb3a9b5924ab0f175a
DanielLorenzoSEP <erick.lorenzo@nube.sep.gob.mx>
2024-05-26 20:35:29 -0600

    UPdate sales esign

--- src/app/pages/sales/pos/pos.component.ts
+++ src/app/pages/sales/pos/pos.component.ts
@@ -17,6 +17,21 @@ export class PosComponent {
     { id: 3, name: 'Product 3', description: "desc", price: 30 },
     { id: 4, name: 'Product 4', description: "desc", price: 40 },
     { id: 5, name: 'Product 5', description: "desc", price: 50 },
+    { id: 6, name: 'Product 6', description: "desc", price: 60 },
+    { id: 7, name: 'Product 7', description: "desc", price: 70 },
+    { id: 8, name: 'Product 8', description: "desc", price: 80 },
+    { id: 9, name: 'Product 9', description: "desc", price: 90 },
+    { id: 10, name: 'Product 10', description: "desc", price: 100 },
+    { id: 11, name: 'Product 11', description: "desc", price: 110 },
+    { id: 12, name: 'Product 12', description: "desc", price: 120 },
+    { id: 13, name: 'Product 13', description: "desc", price: 130 },
+    { id: 14, name: 'Product 14', description: "desc", price: 140 },
+    { id: 15, name: 'Product 15', description: "desc", price: 150 },
+    { id: 16, name: 'Product 16', description: "desc", price: 160 },
+    { id: 17, name: 'Product 17', description: "desc", price: 170 },
+    { id: 18, name: 'Product 18', description: "desc", price: 180 },
+    { id: 19, name: 'Product 19', description: "desc", price: 190 },
+    { id: 20, name: 'Product 20', description: "desc", price: 200 },
   ];
 
   cart = [
