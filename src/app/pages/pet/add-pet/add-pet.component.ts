32b724a6bd06317d71147cfef9a68ac8b351cda4
DanielLorenzoS <daniellorenzo47@aragon.unam.mx>
2024-06-01 18:46:26 -0600

    Commit to ng update

--- src/app/pages/pet/add-pet/add-pet.component.ts
+++ src/app/pages/pet/add-pet/add-pet.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
 import { Observable, map, startWith } from 'rxjs';
 import Pet from 'src/app/models/Pet';
@@ -10,12 +11,24 @@ import { SpinnerService } from 'src/app/services/spinner.service';
 import { UserService } from 'src/app/services/user.service';
 import Swal from 'sweetalert2';
 
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
 @Component({
   selector: 'app-add-pet',
   templateUrl: './add-pet.component.html',
   styleUrls: ['./add-pet.component.css']
 })
 export class AddPetComponent {
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
 
   newPetForm!: FormGroup;
 
